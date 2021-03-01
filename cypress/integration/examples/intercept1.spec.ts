63a67233b480f8654295948a32b7fa999a8fa5f7
Arun Kumar <arunkumar.k@thoughtworks.com>
2021-03-01 10:19:51 +0530

    Updated commit

--- cypress/integration/examples/intercept1.spec.ts
+++ cypress/integration/examples/intercept1.spec.ts
@@ -1,8 +1,13 @@
+import afterEach = Mocha.afterEach;
+
 describe('Network Interception',()=>{
-    it('Sample Intercept 1- Mock the customer page from 10 to 8',()=>{
+    beforeEach(()=>{
         cy.intercept('GET', 'http://localhost:3000/api/customers/page/0/10',{fixture:'page1.json'});
         cy.intercept('GET', 'http://localhost:3000/api/customers/1',{fixture:'mock-order-customer1.json'});
         cy.visit('http://localhost:3000/customers');
+    })
+    it('Sample Intercept 1- Mock the customer page from 10 to 8',()=>{
+
         //Asserting a sample customer from the mock, the mock is present in the fixtures folder by default
         cy.findByTestId('customerGrid').findByText('Ted James').should('exist');
         cy.findByTestId('customerGrid').findByText('Phoenix, Arizona').should('exist');
@@ -16,6 +21,8 @@ describe('Network Interception',()=>{
         //verify the successful redirection
         cy.findByTestId('customerGrid').findByText('Ted James').should('exist');
         cy.findByTestId('customerGrid').findByText('Phoenix, Arizona').should('exist');
-
+    })
+    afterEach(()=>{
+        cy.screenshot();
     })
 })
