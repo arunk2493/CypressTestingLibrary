c4a62962e883a43fe0540f1f9c2f0021c839b81a
Arun Kumar <arunkumar.k@thoughtworks.com>
2021-03-01 09:10:20 +0530

    Updated commit

---
+++ cypress/integration/examples/intercept1.spec.ts
@@ -0,0 +1,21 @@
+describe('Network Interception',()=>{
+    it('Sample Intercept 1- Mock the customer page from 10 to 8',()=>{
+        cy.intercept('GET', 'http://localhost:3000/api/customers/page/0/10',{fixture:'page1.json'});
+        cy.intercept('GET', 'http://localhost:3000/api/customers/1',{fixture:'mock-order-customer1.json'});
+        cy.visit('http://localhost:3000/customers');
+        //Asserting a sample customer from the mock, the mock is present in the fixtures folder by default
+        cy.findByTestId('customerGrid').findByText('Ted James').should('exist');
+        cy.findByTestId('customerGrid').findByText('Phoenix, Arizona').should('exist');
+        cy.findByTestId('customerGrid').findAllByText('View Orders').should('exist').first().click();
+        cy.findByTestId('customerOrderDetails').findByText('Orders for Ted James').should('exist');
+        cy.findByTestId('customerOrderDetails').findByText('Basketball').should('exist');
+        cy.findByTestId('customerOrderDetails').findAllByText('$7.99').first().should('exist');
+        cy.findByTestId('customerOrderDetails').findAllByText('$7.99').first().should('exist');
+        //Redirect to home page
+        cy.findByText('View all Customers').should('exist').click();
+        //verify the successful redirection
+        cy.findByTestId('customerGrid').findByText('Ted James').should('exist');
+        cy.findByTestId('customerGrid').findByText('Phoenix, Arizona').should('exist');
+
+    })
+})
